89e328589e08ee095fdb4f325f0b5c6589e2eb57
Weston Sleeman <weston@wsleeman.com>
2024-07-29 10:38:37 -0700

    Added basic website files.

---
+++ src/Root.fs
@@ -0,0 +1,29 @@
+open System
+open Feliz
+open type Html
+open Browser
+
+[<ReactComponent>]
+let Root () =
+    let name, setName = React.useState "Zori"
+    let title =
+        if String.IsNullOrWhiteSpace(name) then "Nobody" else name.ToUpperInvariant()
+        |> sprintf "%s's Site"
+    let paddedName =
+        if String.IsNullOrWhiteSpace name
+        then ""
+        else sprintf " %s" name
+
+    React.useEffect((fun () -> document.title <- title), [| box name |])
+
+    div [
+        h1 $"Hi{paddedName}! Welcome to your website!"
+        span "Name: "
+        input [
+            prop.value name
+            prop.onChange setName
+        ]
+    ]
+
+let reactRoot = ReactDOM.createRoot(document.getElementById "root")
+reactRoot.render(Root())
