ea47d2ce0ee54a725a057ad64ece3cedad9b6e75
Zori Wang <zori@zorijewelry.com>
2024-08-21 23:08:48 -0700

    Added about page link to home page navbar.

--- src/ui/Navbar.fs
+++ src/ui/Navbar.fs
@@ -1,6 +1,19 @@
 ﻿module Store.UI.Navbar
 
 open Feliz
+open Feliz.Router
+open type Feliz.Html
 
 [<ReactComponent>]
-let Navbar pageRoute = Html.none
+let Navbar pageRoute =
+    ul [
+        li [
+            a [
+                prop.text "About"
+                prop.onClick (fun _ -> Router.navigatePath "/about")
+                prop.style [
+                    style.cursor "pointer"
+                ]
+            ]
+        ]
+    ]
