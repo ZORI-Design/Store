786ec08838abed9272d72230db2c12e8590151ac
Zori Wang <zori@zorijewelry.com>
2024-08-24 12:56:08 -0700

    Fixed code errros.

--- src/ui/NextUI.fs
+++ src/ui/NextUI.fs
@@ -23,7 +23,7 @@ type NextUI =
         Interop.reactApi.createElement (import "Navbar" "@nextui-org/react", createObj !!xs)
 
     static member inline Navbar(xs: ReactElement list) : ReactElement =
-        Interop.reactApi.createElement (import "Navbar" "@nextui-org/react", createObj !![ prop.children xs ])
+        NextUI.Navbar [ prop.children xs ]
 
     static member inline NavbarBrand(xs: IReactProperty list) : ReactElement =
         Interop.reactApi.createElement (import "NavbarBrand" "@nextui-org/react", createObj !!xs)
@@ -31,12 +31,14 @@ type NextUI =
     static member inline NavbarContent(xs: IReactProperty list) : ReactElement =
         Interop.reactApi.createElement (import "NavbarContent" "@nextui-org/react", createObj !!xs)
 
-    static member inline NavbarContent(xs: ReactElement list) : ReactElement = NavbarContent [ prop.children xs ]
+    static member inline NavbarContent(xs: ReactElement list) : ReactElement =
+        NextUI.NavbarContent [ prop.children xs ]
 
     static member inline NavbarItem(xs: IReactProperty list) : ReactElement =
         Interop.reactApi.createElement (import "NavbarItem" "@nextui-org/react", createObj !!xs)
 
-    static member inline NavbarItem(xs: ReactElement list) : ReactElement = NavbarItem [ prop.children xs ]
+    static member inline NavbarItem(xs: ReactElement list) : ReactElement =
+        NextUI.NavbarItem [ prop.children xs ]
 
     static member inline NavbarMenuToggle(xs: IReactProperty list) : ReactElement =
         Interop.reactApi.createElement (import "NavbarMenuToggle" "@nextui-org/react", createObj !!xs)
