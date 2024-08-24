13a465d2a184462334fbd4266178ee9d0d728faf
Zori Wang <zori@zorijewelry.com>
2024-08-24 12:51:00 -0700

    Fixed formatting again.

--- src/ui/NextUI.fs
+++ src/ui/NextUI.fs
@@ -31,14 +31,12 @@ type NextUI =
     static member inline NavbarContent(xs: IReactProperty list) : ReactElement =
         Interop.reactApi.createElement (import "NavbarContent" "@nextui-org/react", createObj !!xs)
 
-    static member inline NavbarContent(xs: ReactElement list) : ReactElement =
-        NavbarContent [ prop.children xs ]
+    static member inline NavbarContent(xs: ReactElement list) : ReactElement = NavbarContent [ prop.children xs ]
 
     static member inline NavbarItem(xs: IReactProperty list) : ReactElement =
         Interop.reactApi.createElement (import "NavbarItem" "@nextui-org/react", createObj !!xs)
 
-    static member inline NavbarItem(xs: ReactElement list) : ReactElement =
-        NavbarItem [ prop.children xs ]
+    static member inline NavbarItem(xs: ReactElement list) : ReactElement = NavbarItem [ prop.children xs ]
 
     static member inline NavbarMenuToggle(xs: IReactProperty list) : ReactElement =
         Interop.reactApi.createElement (import "NavbarMenuToggle" "@nextui-org/react", createObj !!xs)
