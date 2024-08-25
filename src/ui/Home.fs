8e174299c6e55872dbb475ea0a6fc557abbee42a
Zori Wang <zori@zorijewelry.com>
2024-08-24 23:48:47 -0700

    Implemented navbar top style for desktop and mobile.

--- src/ui/Home.fs
+++ src/ui/Home.fs
@@ -6,4 +6,7 @@ open type Feliz.prop
 
 [<ReactComponent>]
 let Home () =
-    h1 [ text "Home"; className [ "text-3xl"; "font-bold"; "underline" ] ]
+    Html.div [
+        prop.style [ style.backgroundColor "#000000"; length.vh 100 |> style.height ]
+        prop.text "Home"
+    ]
--- src/ui/Navbar.fs
+++ src/ui/Navbar.fs
@@ -10,21 +10,18 @@ open Fable.Core.JsInterop
 let DefaultNavbar () =
     let (isMenuOpen, setIsMenuOpen) = React.useState false
 
-    let menuItems = [
-        "Products";
-        "About ZORI";
-        "Contact Us"
-    ]
+    let menuItems = [ "Shop"; "About Us"; "Contact" ]
 
     Navbar [
         prop.custom ("onMenuOpenChange", setIsMenuOpen)
+
         prop.style [
-            style.backgroundColor "rgba(1, 1, 1, 0)"
+            (if isMenuOpen then "#F7F4F0" else "rgba(255,255,255,0)") |> style.backgroundColor
             style.fontFamily "Figtree"
             style.fontWeight 400
-            length.vw 8 |> style.height
+            style.float'.left
             length.px 44 |> style.minHeight
-            length.px 48 |> style.maxHeight
+            (if isMenuOpen then 0.0 else 0.2) |> style.transitionDelaySeconds
         ]
 
         prop.children [
@@ -34,9 +31,10 @@ let DefaultNavbar () =
                         prop.src <| import "default" "./assets/Logo.svg"
                         prop.onClick (fun _ -> Router.navigatePath "/")
                         prop.style [
-                            length.px 24 |> style.height
-                            length.px 24 |> style.minHeight
+                            length.px 26 |> style.height
+                            length.px 26 |> style.minHeight
                             style.cursor "pointer"
+                            (if isMenuOpen then 0 else 100) |> style.filter.invert
                         ]
                     ]
                 ]
@@ -50,28 +48,25 @@ let DefaultNavbar () =
 
                     NavbarItem [
                         Link [
-                            prop.text "Products"
-                            color "foreground"
+                            prop.text "Shop"
                             prop.onClick (fun _ -> Router.navigatePath "/")
-                            prop.style [ style.fontSize 13; style.cursor "pointer" ]
+                            prop.style [ style.fontSize 13; style.cursor "pointer"; style.color "white" ]
                         ]
                     ]
 
                     NavbarItem [
                         Link [
-                            prop.text "About ZORI"
-                            color "foreground"
+                            prop.text "About Us"
                             prop.onClick (fun _ -> Router.navigatePath "/about")
-                            prop.style [ style.fontSize 13; style.cursor "pointer" ]
+                            prop.style [ style.fontSize 13; style.cursor "pointer"; style.color "white" ]
                         ]
                     ]
 
                     NavbarItem [
                         Link [
-                            prop.text "Contact Us"
-                            color "foreground"
+                            prop.text "Contact"
                             prop.onClick (fun _ -> Router.navigatePath "/contact")
-                            prop.style [ style.fontSize 13; style.cursor "pointer" ]
+                            prop.style [ style.fontSize 13; style.cursor "pointer"; style.color "white" ]
                         ]
                     ]
                 ]
@@ -79,7 +74,7 @@ let DefaultNavbar () =
 
             NavbarContent [
                 justify "end"
-                prop.style [ length.px 32 |> style.gap ]
+                prop.style [ length.px 40 |> style.gap ]
                 prop.children [
                     NavbarItem [
                         img [
@@ -87,20 +82,46 @@ let DefaultNavbar () =
                             prop.onClick (fun _ -> Router.navigatePath "/")
                             prop.style [
                                 length.px 21 |> style.height
-                                length.px 21 |> style.minHeight
                                 style.cursor "pointer"
+                                (if isMenuOpen then 0 else 100) |> style.filter.invert
                             ]
                         ]
                     ]
-                    
+
                     NavbarMenuToggle [
                         prop.ariaLabel <| if isMenuOpen then "Close menu" else "Open menu"
                         prop.className "sm:hidden"
+                        prop.style [ (if isMenuOpen then "black" else "white") |> style.color ]
                     ]
                 ]
             ]
 
             NavbarMenu [
+                prop.style [ style.paddingTop 0; style.backgroundColor "#F7F4F0" ]
+                menuItems
+                |> List.indexed
+                |> List.map (fun (index, item) ->
+                    NavbarMenuItem [
+                        prop.key $"{item}-{index}"
+                        prop.children [
+                            Link [
+                                color
+                                <| if index = 2 then "foreground"
+                                   else if index = menuItems.Length - 1 then "danger"
+                                   else "foreground"
+                                prop.className "w-full"
+                                prop.style [
+                                    style.fontFamily "the-seasons"
+                                    length.perc 320 |> style.fontSize
+                                    length.fitContent |> style.height
+                                    (if item = "Shop" then 120 else 30) |> style.paddingTop
+                                ]
+                                prop.text item
+                            ]
+                        ]
+
+                    ])
+                |> prop.children
             ]
         ]
     ]
