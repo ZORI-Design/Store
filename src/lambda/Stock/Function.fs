cf214b4f4622ea2e845a8ad237f7277050a553da
Weston Sleeman <weston@wsleeman.com>
2024-08-29 18:53:34 -0700

    Added stock lambda.

---
+++ src/lambda/Stock/Function.fs
@@ -0,0 +1,53 @@
+namespace Store.Lambdas.Stock
+
+open System
+open Amazon.Lambda.Core
+open FSharp.Json
+open Store.Shared.DomainModel
+open Amazon.Lambda.APIGatewayEvents
+open Store.Crm
+open System.Net
+open Amazon.DynamoDBv2.DocumentModel
+
+[<assembly: LambdaSerializer(typeof<Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer>)>]
+()
+
+type Function() =
+    member __.FunctionHandler (request: APIGatewayHttpApiV2ProxyRequest) (_: ILambdaContext) =
+        try
+            let path = request.RawPath.Trim('/')
+
+            match request.RequestContext.Http.Method.ToUpperInvariant() with
+            | "GET" when path |> String.IsNullOrEmpty ->
+                stock()
+                |> Json.serialize
+                |> fun b -> new APIGatewayHttpApiV2ProxyResponse(
+                    StatusCode = int HttpStatusCode.OK,
+                    Body = b
+                )
+            | "GET" ->
+                genericQuery<Product * Quantity> "Stock" (new QueryFilter("key", QueryOperator.Equal, path))
+                |> Seq.toArray
+                |> Json.serialize
+                |> fun b -> new APIGatewayHttpApiV2ProxyResponse(
+                    StatusCode = int HttpStatusCode.OK,
+                    Body = b
+                )
+            | "PUT" ->
+                match request.PathParameters.TryGetValue "q" with
+                | true, v -> Some v
+                | false, _ -> None
+                |> Option.bind (fun s -> match Int32.TryParse s with | true, i -> Some i | false, _ -> None)
+                |> Option.bind quantity
+                |> Option.bind (fun quantity -> genericQuery<Product * Quantity> "Stock" (new QueryFilter("key", QueryOperator.Equal, path)) |> Seq.tryHead |> Option.map (fun h -> (h, quantity)))
+                |> Option.map (fun ((product, _), quantity) ->
+                    match updateStockQuantity product quantity with
+                    | Ok _ -> new APIGatewayHttpApiV2ProxyResponse(StatusCode = int HttpStatusCode.OK)
+                    | Error e -> new APIGatewayHttpApiV2ProxyResponse(StatusCode = int HttpStatusCode.BadRequest, Body = e)
+                ) |> Option.defaultValue (new APIGatewayHttpApiV2ProxyResponse(StatusCode = int HttpStatusCode.BadRequest, Body = "Invalid request. Is your quantity valid?"))
+            | method -> raise <| new NotSupportedException(method + " is not a supported method.")
+        with ex ->
+            new APIGatewayHttpApiV2ProxyResponse(
+                StatusCode = int HttpStatusCode.BadRequest,
+                Body = ex.ToString() + "\r\n" + ex.StackTrace
+            )
