e4105e6a737f1cc888ecde24194dc2e21f97ebf0
Wes Sleeman <weston@wsleeman.com>
2024-12-07 01:05:33 -0800

    Added responsive breakpointed classes to product page.

--- src/ui/Product.fs
+++ src/ui/Product.fs
@@ -21,10 +21,10 @@ let Product (product: Product) =
     let (selectedImage, setSelectedImage) = React.useState 0
 
     div [
-        className "flex w-full gap-x-4"
+        className "flex w-full xl:w-3/4 xl:mx-auto gap-x-10 gap-y-2 max-sm:flex-col sm:p-8"
         children [
             div [
-                className "flex flex-col w-1/12 gap-y-4"
+                className "flex max-sm:hidden flex-col w-1/12 gap-y-4"
                 children [
                     for (idx, a) in product.Assets |> List.indexed do
                         yield
@@ -38,9 +38,9 @@ let Product (product: Product) =
             ]
 
             div [
-                className "w-1/2"
+                className "w-full sm:w-5/12 flex justify-end flex flex-col gap-y-5"
                 children [
-                    Image [
+                    img [
                         src (
                             if product.Assets.IsEmpty then
                                 product.Thumbnail
@@ -49,11 +49,25 @@ let Product (product: Product) =
                         )
                         className "object-cover aspect-4/5 w-full justify-self-center"
                     ]
+
+                    div [
+                        className "flex sm:hidden w-full gap-2 justify-around"
+                        children [
+                            for (idx, a) in product.Assets |> List.indexed do
+                                yield
+                                    img [
+                                        src a
+                                        className "object-cover aspect-1/1 w-1/6"
+                                        onClick (fun _ -> setSelectedImage idx)
+                                        id idx
+                                    ]
+                        ]
+                    ]
                 ]
             ]
 
             div [
-                className "w-5/12 flex flex-col gap-y-8"
+                className "max-sm:px-5 sm:w-1/2 flex flex-col gap-y-4"
                 children [
                     h1 [ text product.Name; className "text-5xl" ]
                     p [
@@ -70,7 +84,7 @@ let Product (product: Product) =
                     ]
                     Accordion [
                         AccordionItem [
-                            className "w-1/2"
+                            className "xl:w-2/3"
                             custom (
                                 "title",
                                 p [
@@ -97,13 +111,12 @@ let Product (product: Product) =
                         li [ text "Dimensions: ?" ]
                     ]
 
-                    Button [ text "Button"; className "w-1/2" ]
-                    Button [ text "Button"; className "w-1/2" ]
-                    Button [ text "Button"; className "w-1/2" ]
+                    Button [ text "Button"; className "lg:w-2/3" ]
+                    Button [ text "Button"; className "lg:w-2/3" ]
 
                     Button [
-                        text "Stripe"
-                        className "w-1/2"
+                        text "Buy Now"
+                        className "lg:w-2/3"
                         onClick (fun _ ->
                             promise {
                                 let! stripe = stripePromise
